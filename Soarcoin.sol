d8155be48069e27c058fbd05a9ee03baf34ce94c
Micha Roon <micha@roon.me>
2017-03-15 10:15:31 +0100

    interface implementation with the mint function

--- Soarcoin.sol
+++ Soarcoin.sol
@@ -1,23 +1,81 @@
 pragma solidity ^0.4.8;
 
-import "SoarcoinInterface.sol";
-contract Soarcoin {
+/**
+ * ERC 20 token
+ *
+ * https://github.com/ethereum/EIPs/issues/20
+ */
+contract Token {
 
-    mapping (address => uint256) balances;               // each address in this contract may have tokens. 
-    address internal owner = 0x4Bce8E9850254A86a1988E2dA79e41Bc6793640d;                // the owner is the creator of the smart contract
-    string public name = "Soarcoin";                     // name of this contract and investment fund
-    string public symbol = "SOAR";                       // token symbol
-    uint8 public decimals = 6;                           // decimals (for humans)
-    uint256 public totalSupply = 5000000000000000;
-    uint flag = 0;
-           
-    modifier onlyOwner()
-    {
+    /// @return total amount of tokens
+    function totalSupply() constant returns(uint256);
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+    function mint(uint256 _value);
+    
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
+
+contract Owned {
+    address owner;
+
+    function Owned(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
         if (msg.sender != owner) throw;
         _;
     }
 
-    function Soarcoin() { balances[owner] = totalSupply; }    
+
+}
+
+contract SoarcoinImplementation is Owned, Token {
+
+    mapping (address => uint256) balances;               // each address in this contract may have tokens. 
+    string public name = "Soarcoin";                     // name of this contract and investment fund
+    string public symbol = "SOAR";                       // token symbol
+    uint8 public decimals = 6;                           // decimals (for humans)
+    uint256 public totalSupply;
+    uint flag = 0;
+           
+    function Soarcoin(uint256 _totalSupply) { 
+        balances[owner] = _totalSupply; 
+        totalSupply = _totalSupply;
+    }    
+
+    function transferOwnership(address _newOnwer) onlyOwner returns(bool) {
+        owner = _newOnwer;
+    }
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -45,57 +103,71 @@ contract Soarcoin {
 		flag = 1;
 	}	
 	
-    function mint(address _to, uint256 _value) onlyOwner
+    function mint(uint256 _value) onlyOwner
     {
         if(_value <= 0 && flag == 0) throw;
-    	balances[_to] += _value;
+    	balances[msg.sender] += _value;
     	totalSupply += _value;
     }
 }
 
-/**
- * ERC 20 token
- *
- * https://github.com/ethereum/EIPs/issues/20
- */
-contract Token is Soarcoin {
+contract SoarCoin is Owned, Token {
 
-    /// @return total amount of tokens
-    
+    Token implementation;
+
+    function SoarCoin(Token _implementation) {
+        implementation = _implementation;
+    }
+
+    function setImplementation(Token _implementation) onlyOwner {
+        implementation = _implementation;
+    }
+
+    function totalSupply() constant returns(uint256) {
+        return implementation.totalSupply();
+    }
+
+    function mint(uint256 _value) onlyOwner {
+        implementation.mint(_value);
+    }
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return implementation.balanceOf(_owner);
+    }
 
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        return implementation.transfer(_to, _value);
+    }
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        return implementation.transferFrom(_from, _to, _value);
+    }
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        return implementation.approve(_spender, _value);
+    }
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-    
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return implementation.allowance(_owner, _spender);
+    }
 
 }
 
-/**
- * ERC 20 token
- *
